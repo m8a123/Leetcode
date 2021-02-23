9650f82b0ae4fec8c9318a6b877fe90ab5f4063b
m8a123 <43588003+m8a123@users.noreply.github.com>
2021-02-23 16:05:37 -0500

    Create ValidParenthesis.py

---
+++ ValidParenthesis.py
@@ -0,0 +1,31 @@
+    
+def isValid(self, s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    memo = {
+        ')':'(',
+        '}':'{',
+        ']':'['
+    }
+        
+    stack = []
+        
+    for i in s:
+        if i in memo.values():
+            stack.append(i)
+        #check if the pop of the stack is the key of the value
+        if i in memo.keys():
+            if stack == []:
+                return False
+            elif stack.pop() != memo[i]:
+                return False 
+            else:
+                continue 
+            
+            
+    if stack == []:
+        return True 
+    else:
+        return False 
