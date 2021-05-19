fd928b6256ade091383004161d603fdb7e1e1910
m8a123 <43588003+m8a123@users.noreply.github.com>
2021-05-19 11:27:47 -0400

    Create TwoSum.py

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,12 @@
+def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(len(nums)):
+            dict[nums[i]] = i
+        for j in range(len(nums)):
+            if target - nums[j] in dict and dict[target - nums[j]] != j:
+                return [dict[target - nums[j]], j]
